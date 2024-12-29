f7cc98cc875f1aafdfafda0b8a9e629bb78c9105
Neyaz Morshid <neyazmorshid@gmail.com>
2024-12-30 01:32:47 +0600

    first try

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,15 +3,8 @@ import Main from '../layouts/Main'
 import Home from '../pages/Home'
 import Login from '../pages/Authentication/Login'
 import Register from '../pages/Authentication/Register'
-import JobDetails from '../pages/JobDetails'
-import AddJob from '../pages/AddJob'
 import ErrorPage from '../pages/ErrorPage'
-import MyPostedJobs from '../pages/MyPostedJobs'
-import UpdateJob from '../pages/UpdateJob'
-import PrivateRoute from './PrivateRoute'
-import MyBids from '../pages/MyBids'
-import BidRequests from '../pages/BidRequests'
-import AllJobs from '../pages/AllJobs'
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -22,10 +15,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Home />,
       },
-      {
-        path: '/jobs',
-        element: <AllJobs />,
-      },
+     
       {
         path: '/login',
         element: <Login />,
@@ -34,54 +24,7 @@ const router = createBrowserRouter([
         path: '/registration',
         element: <Register />,
       },
-      {
-        path: '/job/:id',
-        element: (
-          <PrivateRoute>
-            <JobDetails />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: '/update/:id',
-        element: (
-          <PrivateRoute>
-            <UpdateJob />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: '/add-job',
-        element: (
-          <PrivateRoute>
-            <AddJob />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: '/my-bids',
-        element: (
-          <PrivateRoute>
-            <MyBids />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: '/my-posted-jobs',
-        element: (
-          <PrivateRoute>
-            <MyPostedJobs />
-          </PrivateRoute>
-        ),
-      },
-      {
-        path: '/bid-requests',
-        element: (
-          <PrivateRoute>
-            <BidRequests />
-          </PrivateRoute>
-        ),
-      },
+     
     ],
   },
 ])
