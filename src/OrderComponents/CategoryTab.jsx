6fa9264fb708ae44a8227d06c8d6c13f0d09146b
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-02 23:41:16 +0600

    in <CategogyTab> we try to implement pagination but not done yet

--- src/OrderComponents/CategoryTab.jsx
+++ src/OrderComponents/CategoryTab.jsx
@@ -1,16 +1,37 @@
 import FoodCard from './FoodCard';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 
 import React from 'react';
 
-const CategoryTab = ({items}) => {
+const CategoryTab = ({ items }) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    };
     return (
         <div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
-                   {
-                        items.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
-                    }
-                   </div>
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+                        {
+                            items.map(item => <FoodCard key={item._id} item={item}></FoodCard>)
+                        }
+                    </div>
+
+
+                </SwiperSlide>
+
+            </Swiper>
+
         </div>
     );
 };
