c328e9c0b12a584523ce909cf158994af4ead79b
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-02 22:50:09 +0600

    module 65 done

--- src/OrderComponents/FoodCard.jsx
+++ src/OrderComponents/FoodCard.jsx
@@ -8,7 +8,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p className="text-center">{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
+                    <button className="btn  bg-yellow-50 hover:text-orange-50 hover:bg-orange-400 border-0 border-orange-400 hover:border-orange-600 text-orange-400 border-b-4 mt-4">Add to Cart</button>
                 </div>
             </div>
         </div>
