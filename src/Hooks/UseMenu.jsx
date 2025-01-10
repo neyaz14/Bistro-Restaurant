16a26849858b3c9cb5929c6267c0ce72f1201ff6
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-10 14:53:05 +0600

    useMenu hooks --> recoded using tanstackQuery

--- src/Hooks/UseMenu.jsx
+++ src/Hooks/UseMenu.jsx
@@ -1,14 +1,28 @@
 import { useEffect, useState } from "react";
+import UseAxiosPublic from "./UseAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 
-const useMenu = ()=>{
- const [menu, setmenu] = useState([]);
- const [loading , setLoading] = useState(true);
-    useEffect(() => {
-        fetch('http://localhost:5000/menu')
-            .then(res => res.json())
-            .then(data => {setmenu(data)})
-    }, [])
-    return [menu, loading]
+const useMenu = () => {
+    //  const [menu, setmenu] = useState([]);
+    //  const [loading , setLoading] = useState(true);
+    //     useEffect(() => {
+    //         fetch('http://localhost:5000/menu')
+    //             .then(res => res.json())
+    //             .then(data => {setmenu(data)})
+    //     }, [])
+    //     return [menu, loading]
+    const axiosPublic = UseAxiosPublic();
+    const { data: menu = [], isPending: loading, refetch } = useQuery({
+        queryKey: ['menu'],
+        queryFn: async () => {
+            const res = await axiosPublic.get('/menu');
+            return res.data;
+        }
+    })
+
+
+    return [menu, loading, refetch]
 }
+
 
 export default useMenu;
