0221df4482e9b89e913c6032beaf6852a6007df5
Neyaz Morshid <niyazmorshid@gmail.com>
2025-01-11 23:19:48 +0600

    adminHome -page done

--- src/pages/Dashboard/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome.jsx
@@ -1,11 +1,63 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import { useQuery } from '@tanstack/react-query';
+import UseAxiosSecure from '../../Hooks/UseAxiosSecure';
+import { FaBook, FaDollarSign, FaUsers } from 'react-icons/fa';
 
 const AdminHome = () => {
-    const {user }= useContext(AuthContext)
+    const { user } = useContext(AuthContext);
+    const axiosSecure = UseAxiosSecure()
+    const { data: stats = {} } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/admin-stats');
+            return res.data;
+        }
+    });
+
     return (
         <div>
-            Admin Home from an admin view-- where he will update his own data
+            <h1 className='text-4xl'> Wellcome <big>{user.displayName}</big></h1>
+            <div className="stats shadow">
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaDollarSign className='text-3xl'></FaDollarSign>
+                    </div>
+                    <div className="stat-title">Revenue</div>
+                    <div className="stat-value">${stats.revenue?.toFixed(3)}</div>
+                    
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaUsers className='text-3xl'></FaUsers>
+                    </div>
+                    <div className="stat-title">Users</div>
+                    <div className="stat-value">{stats.users}</div>
+                    
+                </div>
+
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaBook className='text-3xl'></FaBook>
+                    </div>
+                    <div className="stat-title">Menu Items</div>
+                    <div className="stat-value">{stats.menuItems}</div>
+                    
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
+                    </div>
+                    <div className="stat-title">Orders</div>
+                    <div className="stat-value">{stats.orders}</div>
+                    
+                </div>
+
+            </div>
         </div>
     );
 };
